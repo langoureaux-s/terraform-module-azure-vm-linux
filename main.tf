f4216d69f2bb736a2cedc251ef0f7392c3795539
Sebastien Langoureaux <sebastien.langoureaux@sihm.fr>
2018-05-28 08:35:54 +0000

    replace command by script

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ resource "azurerm_virtual_machine_extension" "custom_command" {
 
     settings = <<SETTINGS
      {
-       "script": "{var.cloudconfig_file == "" ? base64gzip(file("${path.module}/file/custom-script.sh") : base64gzip(file(var.custom_script_path)}"
+       "script": "{var.custom_script_path == "" ? base64gzip(file("${path.module}/file/custom-script.sh")) : base64gzip(file(var.custom_script_path))}"
      }
 SETTINGS
 
