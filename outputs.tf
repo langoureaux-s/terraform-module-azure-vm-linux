bcde0a8d2945ce404354f1e15ba18703421cdb0c
Sebastien Langoureaux <sebastien.langoureaux@sihm.fr>
2018-07-31 15:23:45 +0000

    Force FQDN hostname

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "public_dns_list" {
-  value = "${join(",", azurerm_public_ip.public_ip.*.fqdn)}"
+  value = ["${azurerm_public_ip.public_ip.*.fqdn}"]
 }
 
 output "private_dns_list" {
-  value = "${join(",", azurerm_network_interface.interface.*.internal_fqdn)}"
+  value = ["${azurerm_network_interface.interface.*.internal_fqdn}"]
 }
